e09301344bae67508c48b817b8840dd799ccac8a
Lukas Hlobil <lhlobil@gmail.com>
2017-10-24 19:49:06 +0200

    push

--- lesson3/games.js
+++ lesson3/games.js
@@ -5,14 +5,15 @@ var games = [
     {genre: "strategy", name: 'Starcraft', rating: 9},
     {genre: "FPS", name: "Wolfenstein 3D", rating: 7},
     {genre: "FPS", name: "Call of Duty", rating: 1},
-    {genre: "strategy", name: "Age of Empires", rating: 2},
-    {genre: "FPS", name: "Wolfenstein 3D", rating: 7},
-    {genre: "FPS", name: "Call of Duty", rating: 1}
+    {genre: "strategy", name: "Age of Empires", rating: 2}
 ];
 
 function send() {
-    var name, rating, genre;
-    name = document.getElementById('c_name').value;
-    rating = document.getElementById('c_rating').value;
-    genre = document.getElementById('c_genre').value;
+    var myName, myRating, myGenre, game;
+    myName = document.getElementById('c_name').value;
+    myRating = document.getElementById('c_rating').value;
+    myGenre = document.getElementById('c_genre').value;
+    game = {genre: myGenre, name: myName, rating: myRating};
+    games.push(game); // = games[games.length+1] = game;
+
 }
