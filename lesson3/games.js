d9ee721dcdf40d03cad50f725778c74e63341298
Lukas Hlobil <lhlobil@gmail.com>
2017-11-01 21:31:37 +0100

    forms and stuff

--- lesson3/games.js
+++ lesson3/games.js
@@ -10,6 +10,9 @@ var games = [
 	{genre: 'strategy', name: 'Age of Empires', rating: 2}
 ];
 
+/**
+ * send a game from html
+ */
 function send() {
 	var myName, myRating, myGenre, game;
 	myName = document.getElementById('c_name').value;
@@ -20,6 +23,11 @@ function send() {
 	create(game, 'v_list');
 }
 
+/**
+ * Append a game to html
+ * @param myGame game instance
+ * @param myList string id of html element
+ */
 function create(myGame, myList) {
 	var list = document.getElementById(myList);
 	var listItem = document.createElement('li');
@@ -28,14 +36,20 @@ function create(myGame, myList) {
 	list.appendChild(listItem);
 }
 
+/**
+ * gets called when document is ready
+ */
 function ready() {
 	for (var i = 0; i < games.length; i++) {
 		create(games[i], 'v_list');
 	}
-	worst();
+	worst(0);
 	genreList();
 }
 
+/**
+ * gets games by rating
+ */
 function genreList() {
 	emptyList('v_genre');
 	var genre = document.getElementById('f_genre').value;
@@ -46,6 +60,10 @@ function genreList() {
 	}
 }
 
+/**
+ * clears a html ul
+ * @param listName string id
+ */
 function emptyList(listName) {
 	var myList = document.getElementById(listName);
 	while (myList.hasChildNodes()) {
@@ -53,12 +71,44 @@ function emptyList(listName) {
 	}
 }
 
+/**
+ * gets worst games
+ */
+function worst(insideCount) {
+	emptyList('w_list');
+	var count;
+	if (insideCount) count = insideCount;
+	else count = document.getElementById('s_count').value;
+	sort(games, 'rating');
+	for (var i = 0; i < count; i++)
+		create(games[i], 'w_list');
+}
 
-function worst() {
-	var worstGameEver;
-	for (var i = 0; i < games.length; i++) {
-		if (i < games.length - 1) {
-			if (games[i].rating < games[i + 1].rating) worstGameEver = games[i];
+/**
+ * sorts an array
+ * @param myArray array to sort
+ * @param propertyCheck property of array's element by which the array gets sorted
+ */
+function sort(myArray, propertyCheck) {
+	for (var i = 0; i < myArray.length; i++) {
+		for (var j = i + 1; j < myArray.length; j++) {
+			// game.rating === game['rating']
+			// game.propertyCheck !== game['propertyCheck'] -
+			// game.propertyCheck is tring to find a property named propertyTocCheck on the object
+			// the other syntax is dynamically translating it to a propertyCheck variable value
+			if (myArray[j][propertyCheck] < myArray[i][propertyCheck]) swap(i, j, myArray);
 		}
 	}
+}
+
+/**
+ * swaps elements in array
+ * @param a what to swap
+ * @param b with what
+ * @param arrayToSwap in what array
+ */
+function swap(a, b, arrayToSwap) {
+	var temp = arrayToSwap[a];
+	arrayToSwap[a] = arrayToSwap[b];
+	arrayToSwap[b] = temp;
 }
